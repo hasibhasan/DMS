97d592df5380e80f95ae7a7c0daba9b8c21fbf03
hasib_dt <hhasan@dohatec.net>
2017-03-13 16:44:22 +0600

    structure v2

--- DMS/App_Start/BundleConfig.cs
+++ DMS/App_Start/BundleConfig.cs
@@ -25,7 +25,15 @@ namespace DMS
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/css/bootstrap.css",
                       "~/Content/css/font-awesome.css", 
-                      "~/Content/css/ace-fonts.css", "~/Content/css/ace.css"));
+                      "~/Content/css/ace-fonts.css", "~/Content/css/ace.css"));     
+
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                      "~/Scripts/js/ace/elements.scroller.js", "~/Scripts/js/ace/elements.colorpicker.js", "~/Scripts/js/ace/elements.fileinput.js",
+                       "~/Scripts/js/ace/elements.typeahead.js", "~/Scripts/js/ace/elements.wysiwyg.js", "~/Scripts/js/ace/elements.spinner.js",
+                       "~/Scripts/js/ace/elements.treeview.js", "~/Scripts/js/ace/elements.wizard.js", "~/Scripts/js/ace/elements.aside.js",
+                       "~/Scripts/js/ace/ace.js", "~/Scripts/js/ace/ace.ajax-content.js", "~/Scripts/js/ace/ace.touch-drag.js", "~/Scripts/js/ace/ace.sidebar.js",
+                       "~/Scripts/js/ace/ace.sidebar-scroll-1.js", "~/Scripts/js/ace/ace.submenu-hover.js", "~/Scripts/js/ace/ace.widget-box.js",
+                       "~/Scripts/js/ace/ace.settings.js", "~/Scripts/js/ace/ace.settings-rtl.js", "~/Scripts/js/ace/ace.widget-on-reload.js", "~/Scripts/js/ace/ace.searchbox-autocomplete.js"));
 
         }
     }
---
+++ DMS/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DMS.Controllers
+{
+    public class UserController : Controller
+    {
+        //
+        // GET: /User/
+        [Authorize(Roles = "User")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
